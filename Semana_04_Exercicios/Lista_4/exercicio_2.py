ec2147627c12b04fa53a6e0c897c8df7d24f46c8
Roberta Oliveira <102266840+RobertaOliveira07@users.noreply.github.com>
2024-07-30 20:20:34 -0300

    Lista4 exercicio2

---
+++ Semana_04_Exercicios/Lista_4/exercicio_2.py
@@ -0,0 +1,17 @@
+# 2. Reverso do número. Faça uma função que retorne o reverso de um número inteiro informado. Por exemplo: 127 -> 721.
+
+def reverter_numero(numero):
+    
+    numero_str = str(numero)
+    
+    # Reverte a string
+    numero_reverso_str = numero_str[::-1]
+    
+    # Converte a string revertida de volta para um número inteiro
+    numero_reverso = int(numero_reverso_str)
+    
+    return numero_reverso
+
+
+numero = 127
+print(f"Reverso de {numero} é {reverter_numero(numero)}")
