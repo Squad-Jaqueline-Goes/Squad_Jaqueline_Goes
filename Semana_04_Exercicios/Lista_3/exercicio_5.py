61dc169d1695bf0b7e1ceeaa5d6d0c8a2ee41d1d
alicebelchior <alice.belchior23@gmail.com>
2024-07-30 20:04:47 -0300

    Exercicios das listas da primeira semana

---
+++ Semana_04_Exercicios/Lista_3/exercicio_5.py
@@ -0,0 +1,13 @@
+'''
+Crie duas tuplas. Concatene-as para formar uma nova tupla.
+'''
+
+# criação das tuplas
+tupla1 = ("um", "dois", "três")
+tupla2 = ("somos", "trigêmeas", "sim")
+
+# concatenação das tuplas
+tupla3 = tupla1 + tupla2
+
+#saida de dados
+print(f"Os primeiros versos da abertura de 'As trigêmeas' têm as seguintes palavras:\n{tupla3}")
