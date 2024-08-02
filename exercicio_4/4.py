2e22627c26d99f746d3fc249b1f9790c17c5691c
Fernanda Barbosa <112911182+NandayGB@users.noreply.github.com>
2024-08-02 19:31:31 -0300

    Update 4.py

--- exercicio_4/4.py
+++ exercicio_4/4.py
@@ -4,3 +4,18 @@ pontuação em um exame. O programa deverá solicitar uma nota de 0 a 10. Se
 a pontuação for maior ou igual a 7, o aluno é aprovado; caso contrário, é
 reprovado
 '''
+
+print("Bem-vindo(a) ao sistema de classificação de alunos!")
+
+while True:
+   
+    nota = float(input("Digite a nota do aluno(a) (de 0 a 10): "))
+ 
+    if 0 <= nota <= 10:
+        if nota >= 7:
+            print(f"Nota {nota}: Aluno(a) aprovado!")
+        else:
+            print(f"Nota {nota}: Aluno(a) reprovado.")
+        break  # Sai do loop se a nota for válida
+    else:
+        print("Valor inválido. A nota deve estar entre 0 e 10. Tente novamente.")
