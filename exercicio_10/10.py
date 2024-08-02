59b4fbec898c075c6339eaf468eb5d387ed7cedf
Ana Carolyne <111382055+anadevti@users.noreply.github.com>
2024-08-02 19:50:58 -0300

    Update 10.py

--- exercicio_10/10.py
+++ exercicio_10/10.py
@@ -2,3 +2,13 @@
 Faça um programa que lê três números inteiros e os mostra em ordem
 crescente.
 '''
+
+entrada_1 = int(input('Digite o primeiro numero: '))
+entrada_2 = int(input('Digite o segundo numero: '))
+entrada_3 = int(input('Digite o terceiro numero: '))
+
+numeros = [entrada_1, entrada_2, entrada_3]
+
+ordem = sorted(numeros) # Ordena a lista em ordem crescente
+
+print('Os números em ordem crescente são: ', ordem)
