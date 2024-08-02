b15aff7844e16f4731f45ea6c89c58469ec64dcc
Fernanda Barbosa <112911182+NandayGB@users.noreply.github.com>
2024-08-02 19:34:55 -0300

    Update 5.py

--- exercicio_5/5.py
+++ exercicio_5/5.py
@@ -5,3 +5,20 @@ na string e retorne o total de vogais. Solicite ao usuário para inserir uma
 frase e utilize a função para contar as vogais.
 
 '''
+def contar_vogais(frase):
+    vogais = 'aeiouAEIOU'
+    contador = 0
+    
+    for caractere in frase:
+        if caractere in vogais:
+            contador += 1
+    
+    return contador
+
+def main():
+    frase = input("Digite uma frase: ")
+    total_vogais = contar_vogais(frase)
+    print(f"O total de vogais na frase é: {total_vogais}")
+
+if __name__ == "__main__":
+    main()
