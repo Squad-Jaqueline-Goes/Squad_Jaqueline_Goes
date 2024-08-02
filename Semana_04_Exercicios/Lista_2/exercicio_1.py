b3f0c32203b32abdd1987c074ccf895e7579ac08
anadevti <anacarolayne777@gmail.com>
2024-08-02 18:30:59 -0300

    att repo

--- Semana_04_Exercicios/Lista_2/exercicio_1.py
+++
@@ -1,8 +0,0 @@
-'''
-Faça um Programa que peça dois números e imprima o maior deles
-'''
-
-for i in range(2):
-    numero = int(input('Digite um numero: '))
-if numero >0:
-    print(f'O maior numero é: {numero} ')
