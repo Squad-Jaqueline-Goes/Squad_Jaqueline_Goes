f96e9f560e7c97a648a41bfd772efbed2b9e1950
Fernanda Barbosa <112911182+NandayGB@users.noreply.github.com>
2024-08-02 19:34:13 -0300

    Update 1.py

--- exercicio_1/1.py
+++ exercicio_1/1.py
@@ -3,3 +3,13 @@ Faça um programa, com uma função que necessite de três
 argumentos, e que forneça a soma desses três argumentos.
 
 '''
+def somar_tres_numeros(a, b, c):
+ 
+    return a + b + c
+
+numero1 = 5
+numero2 = 10
+numero3 = 15
+
+resultado = somar_tres_numeros(numero1, numero2, numero3)
+print("A soma dos números é:", resultado)
