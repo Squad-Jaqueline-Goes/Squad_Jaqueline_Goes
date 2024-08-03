40e722b75ed459d81f0d4b51943ed45b7ce4955b
Yasmin Souza Camargo <yasmincamargo012@gmail.com>
2024-08-03 18:16:24 -0300

    exercicio 7 - lista 2

--- exercicio_7/7.py
+++ exercicio_7/7.py
@@ -2,3 +2,18 @@
 Desenvolver um programa que solicite a idade do usuário e identifique se
 ele é uma criança, um adolescente, adulto ou idoso.
 '''
+
+idade = int(input('Digite a sua idade: '))
+
+if idade < 0:
+    print('\nVocê ainda está a caminho!')
+elif idade > 120:
+    print('\nPara esta idade, você deve ser um dinossauro')
+elif idade <= 12:
+    print('\nOlá Criança, não esqueça de brincar hoje!')
+elif idade > 12 and idade <= 18:
+    print('\nOlá Adolescente, não esqueça de estudar')	
+elif idade > 18 and idade <= 60:
+    print('\nOlá Adulto, não esqueça de pagar os boletos')
+else:
+    print('\nIdoso, não esqueça de tomar os remédios')
