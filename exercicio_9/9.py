7a86893ad65a2d695edf77f794e4084f1e09b2bb
ludmylla <ludmyllacaetano1@gmail.com>
2024-08-04 19:00:30 -0300

    Adição dos exercícios 6 e 9 da lista 2

--- exercicio_9/9.py
+++ exercicio_9/9.py
@@ -4,3 +4,16 @@ O programa deve calcular e apresentar a quantidade de números pares e
 informar o valor zero. Certifique-se de incluir validações para garantir que
 apenas números positivos sejam considerados na contagem e cálculos.
 '''
+num = 1
+numeros_pares = 0
+numeros_impares = 0
+
+while(num != 0):
+    num = int(input("Insira um número: "))
+    if (num > 0):
+        if (num%2 == 0):
+            numeros_pares += 1
+        else:
+            numeros_impares += 1
+
+print(f'Foram inseridos {numeros_pares} números pares e {numeros_impares} números ímpares.')
