220829a23e60d7581af15bc81210bb682f375e08
Larissa Albuquerque <106053968+lalbuquerques@users.noreply.github.com>
2024-08-05 13:27:52 -0300

    Update 4.py

--- exercicio_4/4.py
+++ exercicio_4/4.py
@@ -1,5 +1,4 @@
 '''
-<<<<<<< HEAD
 Crie um dicionário representando contatos (nome, telefone).
 Permita ao usuário procurar por um contato pelo nome.
 '''
@@ -21,25 +20,3 @@ for chave in listaTelefonica:
 #Se o usuário não for encontrado exibe uma mensagem.
 if not contatoEncontrado:
   print('Esse contato não está na lista telefônica.')
-=======
-Implemente um programa que classifique um aluno com base em sua
-pontuação em um exame. O programa deverá solicitar uma nota de 0 a 10. Se
-a pontuação for maior ou igual a 7, o aluno é aprovado; caso contrário, é
-reprovado
-'''
-
-print("Bem-vindo(a) ao sistema de classificação de alunos!")
-
-while True:
-   
-    nota = float(input("Digite a nota do aluno(a) (de 0 a 10): "))
- 
-    if 0 <= nota <= 10:
-        if nota >= 7:
-            print(f"Nota {nota}: Aluno(a) aprovado!")
-        else:
-            print(f"Nota {nota}: Aluno(a) reprovado.")
-        break  # Sai do loop se a nota for válida
-    else:
-        print("Valor inválido. A nota deve estar entre 0 e 10. Tente novamente.")
->>>>>>> main
