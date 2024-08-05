d4a59155352af00363b59d04fcc023f44c0e5945
Ludmylla Martins Caetano <87674590+ludmyllacaetano@users.noreply.github.com>
2024-08-05 16:17:48 -0300

    Update 3.py

--- exercicio_3/3.py
+++ exercicio_3/3.py
@@ -1,5 +1,4 @@
 '''
-<<<<<<< HEAD
 Crie um dicionário representando um carrinho de compras.
 Adicione produtos (chaves) e quantidades (valores) ao carrinho.
 Calcule o total do carrinho de compra.
@@ -17,22 +16,3 @@ for valor in carrinho_compras.values():
     total_compra += valor
 
 print(f'O valor total da compra foi de R${total_compra :.2f}')
-=======
-Faça um programa que peça uma nota, entre zero e dez. Mostre uma
-mensagem caso o valor seja inválido e continue pedindo até que o usuário
-informe um valor válido.
-'''
-
-while True:
-    try:
-        nota = float(input('Informe uma nota de 0 a 10: '))
-
-        if 0 <= nota <= 10:
-            break  # Saia do loop se a nota for válida
-        else:
-            print('Valor inválido. A nota deve estar entre 0 e 10.')
-    except ValueError:
-        print('Entrada inválida. Por favor, insira um número.')
-
-print(f'Nota inserida: {nota:.1f}')  # Formatação para uma casa decimal, caso tenha
->>>>>>> main
