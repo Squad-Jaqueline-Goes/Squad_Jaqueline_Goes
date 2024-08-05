a32b70d185ef244f5b84fe8b308e70f78aadc965
Ludmylla Martins Caetano <87674590+ludmyllacaetano@users.noreply.github.com>
2024-08-05 07:58:09 -0300

    Update 4.py

--- exercicio_4/4.py
+++ exercicio_4/4.py
@@ -2,3 +2,10 @@
 Receba do usuário a quantidade de litros de combustível consumidos e
 a distância percorrida. Calcule e imprima o consumo médio em km/l.
 '''
+
+combustivel_consumido = float(input('Informe a quantidade de litros de combustível consumido: '))
+distancia_percorrida = float(input('Informe a distância percorrida em quilômetros: '))
+
+consumo_medio = distancia_percorrida/combustivel_consumido
+
+print(f'O consumo médio foi de {consumo_medio:.2f}km/l')
