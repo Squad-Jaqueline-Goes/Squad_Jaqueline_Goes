845b88dc9ab38462072a82affae830a295e2a449
alicebelchior <alice.belchior23@gmail.com>
2024-08-04 21:31:07 -0300

    Exercício 5 d lista 2

--- exercicio_5/5.py
+++ exercicio_5/5.py
@@ -6,3 +6,17 @@ isósceles: dois lados com o mesmo valor
 escaleno: todos os lados com medidas distintas.
 
 '''
+
+# entrada de dados
+a = int(input("Digite o comprimento do lado A do triângulo: "))
+b = int(input("Digite o comprimento do lado B do triângulo: "))
+c = int(input("Digite o comprimento do lado C do triângulo: "))
+
+
+# saida de dados
+if ((a == b) and (b == c)):
+          print("O triângulo digitado é EQUILÁTERO")
+elif ((a == b) or (a == c) or (b == c)):
+          print("O triângulo digitado é ISÓSCELES")
+else:
+          print("O triângulo digitado é ESCALENO")
