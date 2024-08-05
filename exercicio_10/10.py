3496d8cfe7800376a2520b05e7192528171f812c
Juliana Portela <julianaportehla@hotmail.com>
2024-08-04 21:57:10 -0300

    Exercicios 3 e 10 da lista 1

--- exercicio_10/10.py
+++ exercicio_10/10.py
@@ -7,3 +7,14 @@ também e estou migrando de área.
 Lembrando que para o retorno vamos usar print com as variáveis
 criadas e este texto é somente um exemplo, utilizem a criatividade
 '''
+
+# Solicita as variáveis ao usuário
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+cidade = input("Digite a cidade onde você mora: ")
+profissao = input("Digite sua profissão: ")
+
+# Exibe a mensagem amigável usando as variáveis
+print(f"\nOlá, {nome}! É um prazer te conhecer.")
+print(f"Vejo que você tem {idade} anos e mora em {cidade}.")
+print(f"Como é trabalhar como {profissao}? Deve ser bem interessante!")
